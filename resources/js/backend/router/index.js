480f030679794645e1465b821df325de90490c20
DESKTOP-OR12Q9F\Admin <phetranduy@gmail.com>
2024-08-15 10:09:57 +0700

    init

--- resources/js/backend/router/index.js
+++ resources/js/backend/router/index.js
@@ -4,7 +4,6 @@ import medalsRouter from "./modules/medals";
 import levelsRouter from "./modules/levels";
 import equipmentsRouter from "./modules/equipments";
 import coursesRouter from "./modules/courses";
-import vulanRouter from "./modules/VuLan";
 import vuLanRouter from "./modules/vulan";
 import vuLanDetailRouter from "./modules/detailVuLan";
 import templateVulanRouter from "./modules/templatevulan";
@@ -14,7 +13,6 @@ export const adminRoute = [
     missionsRouter,
     equipmentsRouter,
     coursesRouter,
-    vulanRouter,
     vuLanRouter,
     vuLanDetailRouter,
     templateVulanRouter,
